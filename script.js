e71c7e5d3eaac75b44a6f5c29dab10ca534fb2a3
Thiago Henrique Monserrat Rezende <thiagohenriquemonserrat@gmail.com>
2021-09-30 16:16:09 -0300

    Resolvidoo

--- script.js
+++ script.js
@@ -3,15 +3,17 @@
     document.addEventListener("keydown", (event) => {
       let keyName = event.key;
       if (keyName === "ArrowUp") {
+        document.getElementById("ball").style.backgroundImage = "url('/deidara2.gif')";
         ballTop -= 10;
       } else if (keyName === "ArrowRight") {
-        document.getElementById("ball").style.backgroundImage = "url('/naruto.gif')";
+        document.getElementById("ball").style.backgroundImage = "url('/deidara1.gif')";
         ballLeft += 10;
       } else if (keyName === "ArrowDown") {
+        document.getElementById("ball").style.backgroundImage = "url('/deidara.gif')"
         ballTop += 10;
       } else if (keyName === "ArrowLeft") {
         ballLeft -= 10;
-        document.getElementById("ball").style.backgroundImage = "url('/naruto1.gif')";
+        document.getElementById("ball").style.backgroundImage = "url('/deidara3.gif')";
       } else {
         return alert("USE AS SETAAAS!!");
       }
